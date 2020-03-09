3ff72eacbab8bcab7207828e8d4a30a9f192fa87
Cesar Farrera <cesarfaor@gmail.com>
2020-03-09 13:21:05 -0600

    [Feature] added exercices 2-3

--- tarea03/Tarea03.hs
+++ tarea03/Tarea03.hs
@@ -13,7 +13,7 @@ module Tarea03 where
     
     --reverse
     foldRev :: [a] -> [a]
-    foldRev = foldr (\x tail -> tail ++ [x]) []
+    foldRev = foldr (\x rec -> rec ++ [x]) []
     
     --filter
     foldFltr :: (a -> Bool) -> [a] -> [a]
@@ -25,4 +25,34 @@ module Tarea03 where
     
     --tails
     foldTails :: [a] -> [[a]]
-    foldTails = foldr (\x rec -> ([x] ++ (head rec)):rec) [[]] 
+    foldTails = foldr (\x rec -> ([x] ++ (head rec)):rec) [[]]
+    
+    --Operador de plegado para Int
+    foldi :: (a -> a) -> a -> Int -> a
+    foldi f q 0 = q
+    foldi f q i = f (foldi f q (pred i))
+    
+    --Suma como instancia de foldi (a+b)
+    foldAdd :: Int -> Int -> Int
+    foldAdd a b = foldi (\x -> succ(x)) a b
+    
+    --Multiplicacion como instancia de foldi (a*b)
+    foldMul :: Int -> Int -> Int
+    foldMul a b = foldi (\x -> x+a) 0 b
+
+    --Exponente como instancia de foldi (a^b)
+    foldExpt :: Int -> Int -> Int
+    foldExpt a b = foldi (\x -> x*a) 1 b
+    
+    --Suma de los primeros n naturales al cuadrado
+    sumq :: Int -> Int
+    sumq n = foldl (\x y -> x + y^2) 0 [0..(n-1)]
+    
+    --Elimina los todos los elementos de la segunda lista que aparecian en la primera.
+    remove :: (Eq a) => [a] -> [a] -> [a]
+    remove xs ys = foldr (\x rec -> if elem x xs then rec else (x:rec)) [] ys
+    
+    --Elimina los elementos adyacentes duplicados en una lista
+    remdups :: (Eq a)=> [a] -> [a]
+    remdups l = foldr (\x rec -> if x==(head rec) then rec else (x:rec)) [(last l)] l
+    
